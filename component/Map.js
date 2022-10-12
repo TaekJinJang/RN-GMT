8ad7b8cf9ea0430d12da9f40710e7a839c36a5e2
Taekjin Jang <xorwls1500@naver.com>
2022-10-12 23:39:09 +0900

    subject-1

--- component/Map.js
+++ component/Map.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import * as Location from 'expo-location';
 import { Text, View, StyleSheet, Dimensions } from 'react-native';
-import Styled from 'styled-components/native';
-import BottomTab from './component/BottomTab';
-
 const styles = StyleSheet.create({
   mapStyle: {
     height: Dimensions.get('window').height,
@@ -16,7 +10,7 @@ const styles = StyleSheet.create({
 });
 
 const Tab = createBottomTabNavigator();
-function HomeScreen({ navigation }) {
+function Map({ navigation }) {
   const [mapWidth, setMapWidth] = useState('99%');
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
@@ -78,65 +72,4 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function SearchScreen() {
-  return <Text>Search</Text>;
-}
-
-function NotificationScreen() {
-  return <Text>Notification</Text>;
-}
-
-function MessageScreen() {
-  return <Text>Message</Text>;
-}
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: '홈',
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            title: '지도',
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="map" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Notification"
-          component={NotificationScreen}
-          options={{
-            title: '검색',
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="search" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Message"
-          component={MessageScreen}
-          options={{
-            title: '메시지',
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="message" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
+export default Map;
