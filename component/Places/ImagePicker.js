383235a452a86edb8d458c07279936d3aaff6980
Taekjin Jang <xorwls1500@naver.com>
2022-10-13 22:25:19 +0900

    카메라 권한 성공

--- component/Places/ImagePicker.js
+++ component/Places/ImagePicker.js
@@ -3,45 +3,24 @@ import {
   PermissionStatus,
   useCameraPermissions,
 } from 'expo-image-picker';
+import { Camera } from 'expo-camera';
 import { Alert, Button, View } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import { useEffect } from 'react';
 
 const ImagePicker = () => {
   // 카메라 권한 받아오기
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+    async () => {
+      let { cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      if (cameraStatus !== 'granted') {
+        Alert.alert('이 앱을 사용하려면 카메라 사용 권한이 필요합니다.');
+        return false;
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
+    };
   }, []);
 
-  const [cameraPermissionInformation, requestPermission] =
-    useCameraPermissions();
-
-  async function verifyPermission() {
-    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-      const permissionResponse = await requestPermission();
-      return permissionResponse.granted;
-    }
-    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
-      Alert.alert('이 앱을 사용하려면 카메라 사용 권한이 필요합니다.');
-      console.log(cameraPermissionInformation.status);
-
-      return false;
-    }
-    return true;
-  }
   async function takeImageHandler() {
-    const camera = await verifyPermission({});
-    if (!camera) {
-      return;
-    }
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
